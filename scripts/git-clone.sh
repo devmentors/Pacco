7f4ba80cf6763d27b9bab600ba38871402ff7995
Piotr Gankiewicz <spetz90@gmail.com>
2019-07-11 08:53:47 +0200

    Clone script

---
+++ scripts/git-clone.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+REPOSITORIES=(Pacco Pacco.APIGateway Pacco.Services.Availability Pacco.Services.Customers Pacco.Services.Deliveries Pacco.Services.Identity Pacco.Services.Operations Pacco.Services.Orders Pacco.Services.Parcels Pacco.Services.Pricing Pacco.Services.Vehicles)
+for REPOSITORY in ${REPOSITORIES[*]}
+do
+	 echo ========================================================
+	 echo Cloning repository: $REPOSITORY
+	 echo ========================================================
+	 REPO_URL=https://github.com/devmentors/$REPOSITORY.git
+	 git clone $REPO_URL
+	 cd $REPOSITORY && git checkout workshops && cd ..
+done
