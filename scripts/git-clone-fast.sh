cafc71cfb2dbcadcee6c0148afb2519173a9c113
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2019-09-29 21:04:20 +0200

    updated scripts, compose

--- scripts/git-clone-fast.sh
+++ scripts/git-clone-fast.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-REPOSITORIES=(Pacco Pacco.APIGateway Pacco.Services.Availability Pacco.Services.Customers Pacco.Services.Deliveries Pacco.Services.Identity Pacco.Services.Operations Pacco.Services.Orders Pacco.Services.OrderMaker Pacco.Services.Parcels Pacco.Services.Pricing Pacco.Services.Vehicles)
+REPOSITORIES=(Pacco.APIGateway Pacco.Services.Availability Pacco.Services.Customers Pacco.Services.Deliveries Pacco.Services.Identity Pacco.Services.Operations Pacco.Services.Orders Pacco.Services.OrderMaker Pacco.Services.Parcels Pacco.Services.Pricing Pacco.Services.Vehicles)
 
 echo ${REPOSITORIES[@]} | sed -E -e 's/[[:blank:]]+/\n/g' | xargs -I {} -n 1 -P 0 sh -c 'printf "========================================================\nCloning repository: {}\n========================================================\n"; git clone https://github.com/devmentors/{}.git'
