9a618a70a830be235a2236a8301bbe88e696afdc
Piotr Gankiewicz <piotr.gankiewicz@gmail.com>
2020-04-23 21:07:15 +0200

    Updated PS clone

--- scripts/git-clone.ps1
+++ scripts/git-clone.ps1
@@ -1,5 +1,5 @@
 
-$repos = ("Pacco.APIGateway", "Pacco.Services.Availability", "Pacco.Services.Customers", "Pacco.Services.Deliveries", "Pacco.Services.Identity", "Pacco.Services.Operations", "Pacco.Services.Orders", "Pacco.Services.OrderMaker", "Pacco.Services.Parcels", "Pacco.Services.Pricing", "Pacco.Services.Vehicles")
+$repos = ("Pacco.APIGateway", "Pacco.APIGateway.Ocelot", "Pacco.Services.Availability", "Pacco.Services.Customers", "Pacco.Services.Deliveries", "Pacco.Services.Identity", "Pacco.Services.Operations", "Pacco.Services.Orders", "Pacco.Services.OrderMaker", "Pacco.Services.Parcels", "Pacco.Services.Pricing", "Pacco.Services.Vehicles")
 
 foreach($repo in $repos) {
     Write-Host "=========================================="
